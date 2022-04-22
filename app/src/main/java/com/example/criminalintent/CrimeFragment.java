81706699c299ded0ab77fe400e68f91ef3221061
BDtemp <962614539@qq.com>
2022-04-22 10:44:29 +0800

    update

---
+++ app/src/main/java/com/example/criminalintent/CrimeFragment.java
@@ -0,0 +1,14 @@
+package com.example.criminalintent;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+public class CrimeFragment extends Fragment {
+    private Crime mCrime;
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+    }
+}
