86f7addc1a5da7c3d1a63f4b939e4d7bf5a0260d
wuxinke <962614539@qq.com>
2022-04-24 15:14:54 +0800

    update fragment_crime

--- app/src/main/java/com/example/criminalintent/CrimeLab.java
+++ app/src/main/java/com/example/criminalintent/CrimeLab.java
@@ -1,29 +1,35 @@
 package com.example.criminalintent;
 
 import android.content.Context;
+import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 public class CrimeLab {
+    public static String TAG = "CrimeLab";
     private static CrimeLab sCrimeLab;
     private List<Crime> mCrimes;
 
     public static CrimeLab get(Context context){
         if(sCrimeLab == null){
+            Log.d(TAG,"get");
             sCrimeLab = new CrimeLab(context);
         }
         return sCrimeLab;
     }
     private CrimeLab(Context context){
         mCrimes = new ArrayList<>();
-        for(int i = 0;i<100;++i){
+        for(int i = 0; i<100; i++){
             Crime crime = new Crime();
             crime.setTitle("Crime #" + i);
             crime.setSolved(i % 2 ==0);
             mCrimes.add(crime);
+            Log.d(TAG,crime.getTitle());
         }
+
+
     }
     public List<Crime> getCrimes(){
         return mCrimes;
@@ -31,6 +37,7 @@ public class CrimeLab {
     public Crime getCrime(UUID id){
         for (Crime crime : mCrimes){
             if(crime.getId() == id){
+                Log.d(TAG,crime.getId()+crime.getTitle());
                 return crime;
             }
         }
